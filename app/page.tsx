1766128aec6dd73deb9c24a43f50455cad1a031d
Bogdan <bogdan.pavliv@gmail.com>
2024-07-05 15:52:16 +0300

    add project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 import Choose from "./components/choose";
 import Countries from "./components/countries";
 import About from "./components/about";
